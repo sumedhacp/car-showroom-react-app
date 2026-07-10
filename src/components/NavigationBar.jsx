d0b6e112908b3c88e45f6996cf258266b5a6b0e4
sumedhacp <sumedhastudy2025@gmail.com>
2026-07-10 20:30:03 +0530

    Updated Nav Bar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -7,16 +7,16 @@ const NavigationBar = () => {
 
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#"><img src="" style={{ width: '65px', height: '65px' }} alt="Logo" /></a>
+                    <a class="navbar-brand" href="#"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9ghFFRVMge6JpPzsvZS6JIUGyQXXVwRPE0ijzBsdvRQ&s=10" style={{ width: '65px', height: '65px' }} alt="Logo" /></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div class="navbar-nav">
                             <Link class="nav-link active" aria-current="page" to="/">Add Car</Link>
-                            <Link class="nav-link" to="/search">Search</Link>
-                            <Link class="nav-link" to="/delete">Delete</Link>
-                            <Link class="nav-link" to="/view">View </Link>
+                            <Link class="nav-link" to="/search">Search Car</Link>
+                            <Link class="nav-link" to="/delete">Delete Car</Link>
+                            <Link class="nav-link" to="/view">View Car</Link>
                         </div>
                     </div>
                 </div>
